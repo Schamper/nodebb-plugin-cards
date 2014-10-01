98f0a4057ae046f3c8dc1aff32ddfd0dae2c61f8
Schamper <erikschamper@outlook.com>
2014-10-01 21:46:09 +0200

    No longer cache the template, let the template engine handle everything

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 (function(window) {
-	var delay, cardTpl, targetCard, currentCard, destroyDelay,
+	var delay, targetCard, currentCard, destroyDelay,
 		events = 'mouseenter.card mouseleave.card',
 		exitEvent = 'click.card',
 		selector = 'a[href*="/user/"]:not(.profile-card-img-container a)',
@@ -7,63 +7,58 @@
 		regex = /(\/user\/([^/]+))(?:\/$|$)/;
 
 	$(document).ready(function() {
-		//Preload the card template
-		window.ajaxify.loadTemplate('cards/profile', function(tpl) {
-			cardTpl = tpl;
+		//Listen for events on body, we don't have to re-add on every ajaxify
+		$(document.body).off(events, selector).on(events, selector, function(e) {
+			var target = $(e.currentTarget),
+				href = regex.exec(target.attr('href'));
 
-			//Listen for events on body, we don't have to re-add on every ajaxify
-			$(document.body).off(events, selector).on(events, selector, function(e) {
-				var target = $(e.currentTarget),
-					href = regex.exec(target.attr('href'));
+			//Check if it's a valid link
+			if (href && (!utils.invalidLatinChars.test(href[2]) && !utils.invalidUnicodeChars.test(href[2]))) {
 
-				//Check if it's a valid link
-				if (href && (!utils.invalidLatinChars.test(href[2]) && !utils.invalidUnicodeChars.test(href[2]))) {
+				if (target.children('img')) {
+					//Destroy tooltips added by NodeBB
+					target.children('img').tooltip('destroy');
+				}
 
-					if (target.children('img')) {
-						//Destroy tooltips added by NodeBB
-						target.children('img').tooltip('destroy');
-					}
-
-					if (e.type === "mouseenter" && !target.is(currentCard)) {
-						//If it's a mouseenter event, set a timeout to create a new card
-						delay = setTimeout(function() {
-							createCard(target, href[1]);
+				if (e.type === "mouseenter" && !target.is(currentCard)) {
+					//If it's a mouseenter event, set a timeout to create a new card
+					delay = setTimeout(function() {
+						createCard(target, href[1]);
+					}, 500);
+					targetCard = target;
+				} else {
+					//Otherwise add some handlers for destroying a card
+					if (target.is(currentCard)) {
+						destroyDelay = setTimeout(function() {
+							destroyCard(target);
 						}, 500);
-						targetCard = target;
-					} else {
-						//Otherwise add some handlers for destroying a card
-						if (target.is(currentCard)) {
-							destroyDelay = setTimeout(function() {
-								destroyCard(target);
-							}, 500);
-							target.data('bs.popover')['$tip'].off(events).on(events, function(e) {
-								if (e.type === "mouseenter") {
-									clearTimeout(destroyDelay);
-								} else {
-									destroyDelay = setTimeout(function() {
-										destroyCard(target);
-									}, 500);
-								}
-							});
-						}
-						//Also clear the timeout for creating a new card
-						clearTimeout(delay);
-						targetCard = null;
+						target.data('bs.popover')['$tip'].off(events).on(events, function(e) {
+							if (e.type === "mouseenter") {
+								clearTimeout(destroyDelay);
+							} else {
+								destroyDelay = setTimeout(function() {
+									destroyCard(target);
+								}, 500);
+							}
+						});
 					}
-				}
-			});
-
-			$(window).on('action:ajaxify.start', function() {
-				if (delay) {
+					//Also clear the timeout for creating a new card
 					clearTimeout(delay);
-					delay = 0;
+					targetCard = null;
 				}
-				//Destroy any cards before ajaxifying
-				if (currentCard) {
-					destroyCard(currentCard);
-				}
-				targetCard = null;
-			});
+			}
+		});
+
+		$(window).on('action:ajaxify.start', function() {
+			if (delay) {
+				clearTimeout(delay);
+				delay = 0;
+			}
+			//Destroy any cards before ajaxifying
+			if (currentCard) {
+				destroyCard(currentCard);
+			}
+			targetCard = null;
 		});
 	});
 
@@ -78,47 +73,47 @@
 			url: api,
 			success: function(result) {
 				result.name = result.fullname || result.username;
-				var html = window.templates.parse(cardTpl, result);
-
-				translator.translate(html, function(translated) {
-					//If target is not the currentCard and if the target is the targetCard
-					if (!target.is(currentCard) && target.is(targetCard)) {
-						//If there's an existing card, destroy it
-						if (currentCard) {
-							destroyCard(currentCard);
-						}
-
-						//Create card
-						target.popover({
-							html: true,
-							content: translated,
-							placement: 'top',
-							trigger: 'manual',
-							container: 'body'
-						}).popover('show');
-
-						$('.profile-card-stats li').tooltip();
-
-						utils.makeNumbersHumanReadable($('.profile-card-stats li span'));
-
-						$('.profile-card-chat').off('click.card').on('click.card', function(e) {
-							var card = $(e.currentTarget).parents('.profile-card');
-							app.openChat(card.data('username'), card.data('uid'));
-							return false;
-						});
-
-						currentCard = target;
-
-						$('html').off(exitEvent).on(exitEvent, function() {
+				window.templates.parse('cards/profile', result, function(html) {
+					translator.translate(html, function(translated) {
+						//If target is not the currentCard and if the target is the targetCard
+						if (!target.is(currentCard) && target.is(targetCard)) {
+							//If there's an existing card, destroy it
 							if (currentCard) {
 								destroyCard(currentCard);
 							}
-						});
 
-						$('.profile-card').off(exitEvent).on(exitEvent, function(e) {
-							e.stopPropagation();
-						});
-					}
+							//Create card
+							target.popover({
+								html: true,
+								content: translated,
+								placement: 'top',
+								trigger: 'manual',
+								container: 'body'
+							}).popover('show');
+
+							$('.profile-card-stats li').tooltip();
+
+							utils.makeNumbersHumanReadable($('.profile-card-stats li span'));
+
+							$('.profile-card-chat').off('click.card').on('click.card', function(e) {
+								var card = $(e.currentTarget).parents('.profile-card');
+								app.openChat(card.data('username'), card.data('uid'));
+								return false;
+							});
+
+							currentCard = target;
+
+							$('html').off(exitEvent).on(exitEvent, function() {
+								if (currentCard) {
+									destroyCard(currentCard);
+								}
+							});
+
+							$('.profile-card').off(exitEvent).on(exitEvent, function(e) {
+								e.stopPropagation();
+							});
+						}
+					});
 				});
 			},
 			cache: false
